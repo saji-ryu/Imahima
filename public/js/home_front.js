2577d5e5180695fa80c9c94c15cb8689190af792
saji-ryu <rsajiki@gmail.com>
2018-05-04 15:53:01 +0900

    骨組み完成

--- public/js/home_front.js
+++ public/js/home_front.js
@@ -1,8 +1,23 @@
-$(function () {
-    var ishima = false;
-    $('#himabutton').click(function () {
-        alert('click');
-        ishima = !ishima;
 
+$(function () {
+    var ishima;
+    // console.log(JSON.stringify(window.sessionStorage));
+    $.get('/confirm',function (data) {
+        ishima = data;
+        console.log(ishima);
+        change();
     });
+    $('#himabutton').click(function () {
+        //alert('click');
+        ishima = !ishima;
+        $.post('/',{'IsHima':ishima});
+        change()
+    });
+    function change() {
+        if (ishima) {
+            $('#himabutton').text("今暇");
+        }else{
+            $('#himabutton').text("今暇じゃない");
+        }
+    }
 });
--- src/routes/home.ts
+++ src/routes/home.ts
@@ -4,12 +4,37 @@ const router: express.Router = express.Router();
 
 import {UserModel} from "../model";
 
-router.get('/', function (req, res) {
-
+router.get('/', (req, res) => {
     res.render('home', {
         title: 'login demo',
         session: req.session.passport
     });
+});
+
+router.post('/', (req, res) => {
+    UserModel.findOneAndUpdate(
+        {UserId: req.session.passport.user.id},
+        (req.body),
+        (err, result) => {
+            console.log(result);
+            console.log(req.body);
+            res.send('test');
+        }
+    );
+});
+
+router.get('/confirm', (req, res) => {
+    if (req.session) {
+        UserModel.find({UserId: req.session.passport.user.id}, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(result[0]);
+            res.send(result[0].IsHima);
+        });
+    } else {
+        res.send(false);
+    }
 });
 
 

3f4436c7921a928c56d6238e710937f7bc04312d
saji-ryu <rsajiki@gmail.com>
2018-05-05 16:21:38 +0900

    とりあえず

--- src/client/home.tsx
+++ src/client/home.tsx
@@ -1,5 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-
+class Profile extends React.Component {
+    render() {
+        return (
+            <div className="user_profile">
+                <h1>Shopping List for {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+        );
+    }
+}
 

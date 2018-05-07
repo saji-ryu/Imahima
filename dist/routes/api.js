bb5357dbb1514a566d21de735ee6f559a678fafc
saji-ryu <rsajiki@gmail.com>
2018-05-07 17:28:39 +0900

    js

---
+++ dist/routes/api.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var model_1 = require("../model");
+var router = express.Router();
+router.get('/', function (req, res) {
+    if (req.user) {
+        model_1.UserModel.find({ UserId: req.user.id }, function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            console.log(result[0]);
+            res.send(result[0]);
+        });
+    }
+    else {
+        res.send(null);
+    }
+});
+router.post('/', function (req, res) {
+    model_1.UserModel.findOneAndUpdate({ UserId: req.user.id }, (req.body), function (err, result) {
+        console.log(result);
+        console.log(req.body);
+        res.send('ok');
+    });
+});
+exports.default = router;
+//# sourceMappingURL=api.js.map
